badaaf6aa248aaaaab00c0249e06b7469ab4e815
d1once <dionisenja@gmail.com>
2025-07-12 17:32:18 +0200

    feat: better auth set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,73 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () =>
+    authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
+      },
+      {
+        onRequest: () => {
+          //show loading
+        },
+        onSuccess: () => {
+          alert("Kallamari tytyrityin");
+        },
+        onError: (ctx) => {
+          // display the error message
+          alert(ctx.error.message);
+        },
+      }
+    );
+
+  const { data: session } = authClient.useSession();
+
+  if (session) {
+    return (
+      <div>
+        <p>Kallamari?</p>
+        <p>Yes watashi wa : {session.user.name}</p>
+        <p>Do not go 🥲</p>
+        <p>気をつけて</p>
+        <Button onClick={() => authClient.signOut()}>Sayonara</Button>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" onClick={onSubmit}>
+        Create User
+      </Button>
+    </form>
+  );
 }
